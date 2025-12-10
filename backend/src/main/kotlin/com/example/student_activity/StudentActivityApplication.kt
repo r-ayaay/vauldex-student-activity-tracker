1b3e34c2cc62323219ebbd86b9821b9986b70093
Raymond Anthony Aya-ay <54580181+r-ayaay@users.noreply.github.com>
2025-12-10 09:21:03 +0800

    initial commit with fresh springboot and vue installation

---
+++ backend/src/main/kotlin/com/example/student_activity/StudentActivityApplication.kt
@@ -0,0 +1,11 @@
+package com.example.student_activity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudentActivityApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudentActivityApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/example/student_activity/StudentActivityApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.student_activity
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudentActivityApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
